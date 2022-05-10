a3d8d0407d2ab5677239286445930433d82456e0
YaroslavKov <koval.yar.val@gmail.com>
2022-05-10 10:40:18 +0300

    +all endpoints

--- errors.go
+++ errors.go
@@ -3,7 +3,8 @@ package main
 import "errors"
 
 var (
-	personExistError    = errors.New("person already exist")
-	notValidPersonError = errors.New("person not valid")
-	wrongContentType    = errors.New("wrong content type")
+	personExistError      = errors.New("person already exist")
+	notValidPersonError   = errors.New("person not valid")
+	wrongContentTypeError = errors.New("wrong content type")
+	invalidUuidError      = errors.New("invalid uuid")
 )
--- in_memory_person_storage.go
+++ in_memory_person_storage.go
@@ -11,8 +11,8 @@ func NewInMemoryPersonStorage() *InMemoryPersonStorage {
 }
 
 func (s *InMemoryPersonStorage) Add(person *Person) error {
-	_, ok := s.data[person.ID]
-	if ok {
+	p := s.GetPersonById(person.ID)
+	if p != nil {
 		return personExistError
 	}
 
@@ -26,4 +26,40 @@ func (s *InMemoryPersonStorage) GetAll() []*Person {
 		persons = append(persons, person)
 	}
 	return persons
+}
+
+func (s *InMemoryPersonStorage) GetPersonById(id uuid.UUID) *Person {
+	p, ok := s.data[id]
+	if !ok {
+		return nil
+	}
+	return p
+}
+
+func (s *InMemoryPersonStorage) GetPersonsByName(name string) []*Person {
+	persons := make([]*Person, 0)
+	for _, val := range s.data {
+		if val.Name == name {
+			persons = append(persons, val)
+		}
+	}
+	return persons
+}
+
+func (s *InMemoryPersonStorage) UpdatePerson(person *Person) bool {
+	p := s.GetPersonById(person.ID)
+	if p == nil {
+		return false
+	}
+	s.data[person.ID] = person
+	return true
+}
+
+func (s *InMemoryPersonStorage) DeletePerson(id uuid.UUID) bool {
+	p := s.GetPersonById(id)
+	if p == nil {
+		return false
+	}
+	delete(s.data, id)
+	return true
 }
